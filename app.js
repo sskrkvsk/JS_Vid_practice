1033b67b957d98837fa8557f9aeda02b960a5993
sskrkvsk <62514297+sskrkvsk@users.noreply.github.com>
2021-06-24 05:58:36 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+   // get random number between 0 and 3
+   const randomNumber = getRandomNumber();
+   document.body.style.backgroundColor = colors[randomNumber];
+   color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+   return Math.floor(Math.random() * colors.length);
+}
+
+function copyIt() {
+   window.getSelection().selectAllChildren(color);
+   document.execCommand("copy");
+
+   /* Alert the copied text */
+   alert("Copied the text: " + color.innerHTML);
+}
+color.addEventListener("click", copyIt);
